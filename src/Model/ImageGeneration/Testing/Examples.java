3af836ee7714f688eb829de629740c011826dfda
Josh Beck <j.beck18@me.com>
2018-12-01 23:02:49 -0500

    Added examples

--- src/Model/ImageGeneration/Testing/Examples.java
+++ src/Model/ImageGeneration/Testing/Examples.java
@@ -8,24 +8,24 @@ import Model.ImageGeneration.Preferences.VCAttributes;
 public class Examples {
 
     public static void main(String[] args) {
-        VCAttributes preferences = new VCAttributes();
-        VisualCube.saveImageWithPreferencesToFile(preferences, "examples/example_1");
+        VCAttributes attributes = new VCAttributes();
+        VisualCube.saveImageWithAttributesToFile(attributes, "examples/example_1");
 
-        preferences = new VCAttributes.Builder()
+        attributes = new VCAttributes.Builder()
                 .backgroundColor(BackgroundColor.TRANSPARENT)
                 .algorithm("S2 E2 M2")
                 .imageSize(250)
                 .build();
 
-        VisualCube.saveImageWithPreferencesToFile(preferences, "examples/example_2");
+        VisualCube.saveImageWithAttributesToFile(attributes, "examples/example_2");
 
-        preferences = new VCAttributes.Builder()
+        attributes = new VCAttributes.Builder()
                 .backgroundColor(BackgroundColor.TRANSPARENT)
                 .algorithmCase("R U R'")
                 .stageMask(StageMaskType.F2L)
                 .imageSize(250)
                 .build();
 
-        VisualCube.saveImageWithPreferencesToFile(preferences, "examples/example_3");
+        VisualCube.saveImageWithAttributesToFile(attributes, "examples/example_3");
     }
 }
